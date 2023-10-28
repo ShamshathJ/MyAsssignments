79fda9ff88a20668848f3840939363432b275dfb
ShamshathJ <149229475+ShamshathJ@users.noreply.github.com>
2023-10-28 12:50:41 +0530

    Week1 Day 1

---
+++ week1Day1/SampleProgram.java
@@ -0,0 +1,13 @@
+package week1Day1;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to TestLeaf");
+		
+		
+
+	}
+
+}
